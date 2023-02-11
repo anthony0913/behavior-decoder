47ad5859ac99da2a3e6f56c116eee2bc30da65b2
Anthony <44311909+anthony0625@users.noreply.github.com>
2023-02-12 04:11:15 +0900

    separation of parameter cleaning into Batcher class

--- optimized/main.py
+++ optimized/main.py
@@ -9,15 +9,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 class Optimizer:
-    def __init__(self, data, params, freqs, output_column, mode="power", lower_freq, upper_freq,):
-        self.data = data
-        self.params = params
+    def __init__(self, data, params, freqs, constraints, mode="power", lower_freq=2, upper_freq=40):
+        self.data = data #Array containing time series data about the total session
+        self.params = params #Array containing cleaned trial parameters
 
     def process_data(self):
         return 0
 
-    def clean_params(self):
-        return 0
-
     def optimize(self):
         return 0
+
+class Batcher:
+    def __init__(self, output_column=2):
+        print()
