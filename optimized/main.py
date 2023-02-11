1d4b43ce2b5f636b7f64fd80fc7647c5ce25c0d2
Anthony <44311909+anthony0625@users.noreply.github.com>
2023-02-12 04:38:56 +0900

    partial implementation of optimization

--- optimized/main.py
+++ optimized/main.py
@@ -20,12 +20,18 @@ class Optimizer:
         primary = np.zeros((np.shape(data)[0],np.shape(data)[1],np.shape(params)[0]))
         for trial in range(np.shape(params)[0]):
             gamma = data[params[0],params[1],:]
-            gamma = np.real(np.fft.fft(gamma))
+            gamma = np.real(np.fft.fft(gamma, axis=0))
             if scheme=="power":
                 primary[:,:,trial] = gamma[self.freqs,:]
+        #rescale before returning
         return primary
 
     def optimize(self):
+        training_input, testing_input, training_output, testing_output = train_test_split(
+            data, exp_out, test_size=0.25, stratify = exp_out
+        )
+        testing_input = np.vstack((testing_input, extra_data))
+        testing_output = np.hstack((testing_output, extra_exp_out))
         return 0
 
 class Batcher:
