ce24818925612daa9fb80f4f523c8e4179e175d3
Anthony <44311909+anthony0625@users.noreply.github.com>
2023-03-06 02:27:51 +0900

    fixed same block issue

--- visualization.py
+++ visualization.py
@@ -5,8 +5,9 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 
 class Clean:
-    def __init__(self, params_file):
+    def __init__(self, params_file, min_length):
         self.params_file = params_file
+        self.min_length = min_length
         self.cleaned_params = None
         self.cleaned_params_1 = None
         self.cleaned_params_0 = None
@@ -16,8 +17,9 @@ class Clean:
         with open(self.params_file, 'r') as params_csv:
             for row in csv.reader(params_csv):
                 if row[1] == "FTP" and row[4] == "clean":
-                    cleaned_row = [1 if row[3] == "correct" else 0, int(row[6]), int(row[7])]
-                    cleaned_params.append(cleaned_row)
+                    if (int(row[7])-int(row[6]))>self.min_length:
+                        cleaned_row = [1 if row[3] == "correct" else 0, int(row[6]), int(row[7])]
+                        cleaned_params.append(cleaned_row)
         self.cleaned_params = np.array(cleaned_params, dtype=int)
 
     def separate_cleaned_params(self):
@@ -49,25 +51,30 @@ class Transform:
         self.blocks_0 = None
 
     def get_blocks(self, cleaned_params, normalize=False):
-        blocks = []
-        with open(self.session_file, 'r') as session_csv:
-            session_reader = csv.reader(session_csv)
-            next(session_reader)
-            for row in session_reader:
-                session_col = int(row[0])
-                for cleaned_row in cleaned_params:
-                    if session_col >= cleaned_row[1] and session_col <= cleaned_row[2]:
-                        block = np.array(row[1:], dtype=float)
-                        blocks.append(block)
-        blocks_ft = np.fft.rfft(blocks, axis=0).real
-        trimmed_blocks = blocks_ft[:10, :]
-        stacked_blocks = np.stack([trimmed_blocks] * cleaned_params.shape[0], axis=2)
+        with open(self.session_file, newline='') as f:
+            reader = csv.reader(f, delimiter=',')
+            data_array = [row for row in reader][1:]
+        data = np.array(data_array, dtype=np.float32)[:, 1:]
+
+        blocks = np.empty(0)
+        for cleaned_row in cleaned_params:
+            block = data[cleaned_row[1]:cleaned_row[2],:]
+            block = np.fft.fft(block, axis=0)
+            block = block.real[self.freqs, :]
+            if blocks.ndim == 3:
+                blocks = np.concatenate((blocks, block[:,:,np.newaxis]), axis=2)
+            elif blocks.ndim == 2:
+                blocks = np.dstack((blocks, block))
+            else:
+                blocks = block
+        stacked_blocks = blocks
         if normalize:
             stacked_blocks -= stacked_blocks.min(axis=(0, 1), keepdims=True)
             stacked_blocks /= stacked_blocks.max(axis=(0, 1), keepdims=True)
         return stacked_blocks
 
-    def generate_heatmaps(self, cleaned_params_1, cleaned_params_0, output_file, normalize=False):
+    def generate_heatmaps(self, cleaned_params_1, cleaned_params_0, output_file, freqs, normalize=False):
+        self.freqs = freqs
         blocks_1 = self.get_blocks(cleaned_params_1, normalize)
         blocks_0 = self.get_blocks(cleaned_params_0, normalize)
 
@@ -86,12 +93,14 @@ class Transform:
                 pdf.savefig(fig)
                 plt.close()
 
-
-clean = Clean('params.csv')
+clean = Clean('params.csv', 9)
 transform = Transform('data.csv')
+output_file = 'heatmaps.pdf'
 
 cleaned_params_1 = clean.get_cleaned_params_1()
 cleaned_params_0 = clean.get_cleaned_params_0()
 
-output_file = 'heatmaps.pdf'
-transform.generate_heatmaps(cleaned_params_1, cleaned_params_0, output_file)
+#heatmap = Heatmaps("data.csv", "params.csv", freqs=[0,1,2,4,5,8,9], output_file=output_file)
+#transform.generate_heatmaps(cleaned_params_1, cleaned_params_0, output_file, freqs=np.arange(1,10))
+#transform.generate_heatmaps(cleaned_params_1, cleaned_params_0, "heatmaps2.pdf", freqs=[0,1,2,4,5,8,9])
+transform.generate_heatmaps(cleaned_params_1, cleaned_params_0, "heatmaps2.pdf", freqs=[3])
